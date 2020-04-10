9fc33c5e3ff4bd9f5eb08cbf390de0e3a96f1980
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-10 14:06:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ end
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   pioneer = {
-    :name = 'Grace Hopper'
+    :name => 'Grace Hopper'
   }
 end
 
